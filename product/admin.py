bbb9250e2dff1c3080dc46a85b8b9d6941061864
Svi artur <sviartur8@gmail.com>
2025-04-03 19:29:49 +0300

    lok

--- product/admin.py
+++ product/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from product.models import Product
 
-admin.site.register(Product)
+# admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['name','descriptions','price','company']
+    search_fields=['name']
